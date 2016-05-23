f63fc600598cccb3631e2668131b640fd911d9fb
john lester lucena <john.lester.f.lucena@accenture.com>
2016-05-23 14:03:36 +0800

    Added Set 11,12 and 13

---
+++ jenkins/jobs/dsl/set11and12.groovy
@@ -0,0 +1,598 @@
+// Folders
+def workspaceFolderName = "${WORKSPACE_NAME}"
+def projectFolderName = "${PROJECT_NAME}"
+def hfm_FolderName = projectFolderName + "/HCM_Features_Manager"
+def set11_FolderName = hfm_FolderName + "/Set_11_and_12"
+
+// Repositories
+def hcmSet11Config = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/HCM_Set_11_12_Config"
+def hcmApp = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/HCM_App_Repo"
+
+// Jobs
+def retrieveConfig = freeStyleJob(set11_FolderName + "/Retrieve_Configuration")
+def workpatterns = freeStyleJob(set11_FolderName + "/Manage_Work_Workday_Patterns")
+def worksched = freeStyleJob(set11_FolderName + "/Manage_Work_Schedules")
+def workadminstration = freeStyleJob(set11_FolderName + "/Manage_Work_Schedule_Assignment_Administration")
+def workoforceoptionvalues = freeStyleJob(set11_FolderName + "/Manage_Workforce_Records_Profile_Option_Values")
+def nameformats = freeStyleJob(set11_FolderName + "/Manage_Person_Name_Formats")
+def persontypes = freeStyleJob(set11_FolderName + "/Manage_Person_Types")
+def assignmentstatus = freeStyleJob(set11_FolderName + "/Manage_Assignment_Status")
+def documenttypes = freeStyleJob(set11_FolderName + "/Manage_Document_Types")
+def eligibility = freeStyleJob(set11_FolderName + "/Manage_Eligibility_Profiles")
+def checklisttemplate = freeStyleJob(set11_FolderName + "/Manage_Checklist_Template")
+
+// Pipeline
+def usecase11_pipeline = buildPipelineView(set11_FolderName + "/Set_11_and_12")
+
+usecase11_pipeline.with{
+    title('Set 11 and 12')
+    displayedBuilds(5)
+    selectedJob(set11_FolderName + "/Retrieve_Configuration")
+    showPipelineParameters()
+    refreshFrequency(5)
+}
+
+
+// Set 11 and 12 jobs
+
+retrieveConfig.with{
+    description("This retrieves the configuration file that will be used as a template for use case set 2 to the Oracle HCM Application")
+    wrappers {
+    preBuildCleanup()
+    sshAgent("adop-jenkins-master")
+  }
+  scm{
+    git{
+      remote{
+        url(hcmSet11Config)
+        credentials("adop-jenkins-master")
+      }
+      branch("*/master")
+    }
+  }
+  environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+  }
+  publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Work_Workday_Patterns"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+workpatterns.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Work_Workday_Patterns/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Work Workday Patterns" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Work_Schedules"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+worksched.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+    steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Work_Schedules/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Work Schedules" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Work_Schedule_Assignment_Administration"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+workadminstration.with {
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+	scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+	 steps {
+        maven{
+          rootPOM('pom.xml')
+          goals('clean install')
+          mavenInstallation("ADOP Maven")
+        }
+		
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Work_Schedule_Assignment_Administration/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Work Schedule Assignment Administration" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+    }
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Workforce_Records_Profile_Option_Values"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+workoforceoptionvalues.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Workforce_Records_Profile_Option_Values/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Workforce Records Profile Option Values" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE	
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Person_Name_Formats"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+nameformats.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Person_Name_Formats/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Person Name Formats" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Person_Types"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+persontypes.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Person_Types/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Person Types" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE	
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Assignment_Status"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+assignmentstatus.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Assignment_Status/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Assignment Status" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE	
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Document_Types"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+documenttypes.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Document_Types/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Document Types" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Eligibility_Profiles"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+eligibility.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Eligibility_Profiles/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Eligibility Profiles" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set11_FolderName + "/Manage_Checklist_Template"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+checklisttemplate.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Manage_Checklist_Template/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Checklist Template" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_11_and_12/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+}
---
+++ jenkins/jobs/dsl/set13.groovy
@@ -0,0 +1,322 @@
+// Folders
+def workspaceFolderName = "${WORKSPACE_NAME}"
+def projectFolderName = "${PROJECT_NAME}"
+def hfm_FolderName = projectFolderName + "/HCM_Features_Manager"
+def set13_FolderName = hfm_FolderName + "/Set_13""
+
+// Repositories
+def hcmSet13Config = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/HCM_Set13_Config"
+def hcmApp = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/HCM_App_Repo"
+
+// Jobs
+def retrieveConfig = freeStyleJob(set13_FolderName + "/Retrieve_Configuration")
+def consolidationgrp = freeStyleJob(set13_FolderName + "/Manage_Consolidation_Groups")
+def fastformulas = freeStyleJob(set13_FolderName + "/Manage_Fast_Formulas")
+def elements = freeStyleJob(set13_FolderName + "/Manage_Elements")
+def roleprovisioning = freeStyleJob(set13_FolderName + "/Manage_Role_Provisioning_Rules")
+def commonlookups = freeStyleJob(set13_FolderName + "/Manage_Common_Lookups")
+
+// Pipeline
+def usecase13_pipeline = buildPipelineView(set13_FolderName + "/Set_13")
+
+usecase13_pipeline.with{
+    title('Set 13')
+    displayedBuilds(5)
+    selectedJob(set13_FolderName + "/Retrieve_Configuration")
+    showPipelineParameters()
+    refreshFrequency(5)
+}
+
+// Set 13
+
+retrieveConfig.with{
+    description("This retrieves the configuration file that will be used as a template for use case set 2 to the Oracle HCM Application")
+    wrappers {
+    preBuildCleanup()
+    sshAgent("adop-jenkins-master")
+  }
+  scm{
+    git{
+      remote{
+        url(hcmSet13Config)
+        credentials("adop-jenkins-master")
+      }
+      branch("*/master")
+    }
+  }
+  environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+  }
+  publishers{
+    downstreamParameterized{
+      trigger(set13_FolderName + "/Manage_Consolidation_Groups"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+consolidationgrp.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Manage_Consolidation_Groups/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Consolidation Groups" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set13_FolderName + "/Manage_Fast_Formulas"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+fastformulas.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+    steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Manage_Fast_Formulas/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Fast Formulas" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set13_FolderName + "/Manage_Elements"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+elements.with {
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+	scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+	 steps {
+        maven{
+          rootPOM('pom.xml')
+          goals('clean install')
+          mavenInstallation("ADOP Maven")
+        }
+		
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Manage_Elements/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Elements" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+    }
+	publishers{
+    downstreamParameterized{
+      trigger(set13_FolderName + "/Manage_Role_Provisioning_Rules"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+roleprovisioning.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Manage_Role_Provisioning_Rules/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Role Provisioning Rules" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE	
+		''')
+	}
+	publishers{
+    downstreamParameterized{
+      trigger(set13_FolderName + "/Manage_Common_Lookups"){
+        condition("SUCCESS")
+		  parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+commonlookups.with{
+	parameters {
+		stringParam("B","","Build Number")
+		stringParam("PARENT_BUILD","","Parent Build Job")
+	}
+    wrappers {
+        preBuildCleanup()
+        sshAgent("adop-jenkins-master")
+    }
+    scm{
+        git{
+            remote{
+                url(hcmApp)
+                credentials("adop-jenkins-master")
+            }
+            branch("*/master")
+        }
+    }
+    environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+    }
+     steps {
+		maven{
+		rootPOM('pom.xml')
+		goals('clean install')
+		mavenInstallation("ADOP Maven")
+	}
+		shell('''#!/bin/bash
+java -jar /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Manage_Common_Lookups/workspace/target/HCM-0.0.1-SNAPSHOT.jar -r "Manage Common Lookups" -w $WORKSPACE -e /var/jenkins_home/jobs/Oracle/jobs/HCM/jobs/HCM_Features_Manager/jobs/Set_13/jobs/Retrieve_Configuration/workspace
+cd ..
+mkdir screenshots 
+cd screenshots       
+cp -avr $WORKSPACE/target/screenshots/* .
+cd ..
+rm -rf $WORKSPACE/*
+rm -rf $WORKSPACE/.git $WORKSPACE/.settings
+rm -f $WORKSPACE/.classpath $WORKSPACE/.project
+mv screenshots $WORKSPACE
+		''')
+	}
+}
